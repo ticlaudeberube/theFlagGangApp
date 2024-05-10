eb606a5c168f62a44407a13e6916592b96ceea4d
Claude Berube <claude.berube@gmail.com>
2024-05-10 16:17:33 -0400

    main changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:theflaggangapp/views/login_view.dart';
+import 'package:theflaggangapp/firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +11,50 @@ void main() {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       useMaterial3: true,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Home'),
+        ),
+        body: FutureBuilder(
+            future: Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    print('You are a verified user');
+                  } else {
+                    print('You need to verify your email address');
+                  }
+                  return const Text('Done');
+                default:
+                  return const Text('Loading...');
+              }
+            }));
+  }
+}
+
+class _VerifyEmailViewState extends StatefulWidget {
+  const _VerifyEmailViewState(Key? key) : super(key: key);
+
+  @override
+  State<_VerifyEmailViewState> createState() => __VerifyEmailViewStateState();
+}
+
+class __VerifyEmailViewStateState extends State<_VerifyEmailViewState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
 }
